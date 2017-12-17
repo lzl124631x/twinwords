f02660536e07589ab9bdae28aee0be8ad53a28ab
Richard LIU <liuzhenglaichn@gmail.com>
2017-12-17 21:28:26 +0000

    updated apiUrl in appjs

--- front-end/app.js
+++ front-end/app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static('dist'))
 
-var apiUrl = 'http://localhost:3000'
+var apiUrl = 'http://localhost:3000/api'
 app.use('/api', function(req, res) {
   var url = apiUrl + req.url;
   req.pipe(request(url)).pipe(res);
